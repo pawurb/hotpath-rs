1d7569eef20d4a2c021933c4a25336e71f1783e6
pawurb <contact@pawelurbanek.com>
2026-02-18 13:02:39 +0100

    fix: Pass args to default cmd

--- crates/hotpath/bin/hotpath/main.rs
+++ crates/hotpath/bin/hotpath/main.rs
@@ -16,24 +16,24 @@ pub enum HPSubcommand {
     about,
     long_about = "hotpath CLI: automatically profile Rust programs on each Pull Request
 
-https://github.com/pawurb/hotpath-rs"
+https://github.com/pawurb/hotpath-rs",
+    args_conflicts_with_subcommands = true
 )]
 pub struct HPArgs {
     #[command(subcommand)]
     pub cmd: Option<HPSubcommand>,
+
+    #[command(flatten)]
+    pub console_args: ConsoleArgs,
 }
 
 #[hotpath::main(limit = 20)]
 fn main() -> Result<()> {
     let root_args = HPArgs::parse();
 
-    match root_args
-        .cmd
-        .unwrap_or(HPSubcommand::Console(ConsoleArgs::default()))
-    {
-        HPSubcommand::Console(args) => {
-            args.run()?;
-        }
+    match root_args.cmd {
+        Some(HPSubcommand::Console(args)) => args.run()?,
+        None => root_args.console_args.run()?,
     }
 
     Ok(())
