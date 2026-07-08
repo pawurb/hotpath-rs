f85b728a39530e19ebcc42c11adb95ca48c601d9
Paweł Urbanek <contact@pawelurbanek.com>
2026-07-08 23:07:57 +0200

    fix: Fix passing hotpath-alloc to macro crate

---
+++ crates/test-custom-feature/examples/custom_feature.rs
@@ -0,0 +1,24 @@
+use std::time::Duration;
+
+#[hotpath::measure]
+fn allocating_function(sleep: u64) {
+    let vec = vec![
+        1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ];
+    std::hint::black_box(&vec);
+    std::thread::sleep(Duration::from_nanos(sleep));
+}
+
+#[tokio::main(flavor = "current_thread")]
+#[hotpath::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    for i in 0..100 {
+        allocating_function(i);
+    }
+
+    if let Ok(secs) = std::env::var("TEST_SLEEP_SECONDS") {
+        tokio::time::sleep(Duration::from_secs(secs.parse()?)).await;
+    }
+
+    Ok(())
+}
